6a765f4f963de48bf778673d9e1022b39504f045
root <root@DESKTOP-CLDBGJS.localdomain>
2022-06-01 20:45:32 +0300

    s

--- Newspaper/NewsPortal/tasks.py
+++ Newspaper/NewsPortal/tasks.py
@@ -1,7 +1,32 @@
 from celery import shared_task
-import time
+from django.core.mail import EmailMultiAlternatives
+from NewsPortal.models import * 
+from django.template.loader import *
+from datetime import datetime
+
 
 @shared_task
-def hello():
-    time.sleep(10)
-    print("Hello, world!")
+def send_weekly_mail():
+    for category in Category.objects.all():
+        news_from_each_category = []
+        last_week = datetime.now().isocalendar()[1] - 1
+        for news in Post.objects.filter(postCategory=category.id,
+            creation_timedate__week=last_week).values('pk', 'title', 'creation_timedate', 'postCategory__name'):
+            date_format = news.get("creation_timedate").strftime("%m/%d/%Y")
+            article =   (f'http://127.0.0.1:8000/news/{news.get("pk")}, {news.get("title")},'
+                         f'Category: {news.get("postCategory__name")}, Date: {date_format}')
+            news_from_each_category.append(article)
+            
+    for subscriber in category.subscribers.all():
+        html_content = render_to_string(
+                'weekly.html', {'user': subscriber,
+                                'text': news_from_each_category,
+                                'category_name': category.name,
+                                'last_week': last_week})
+        msg = EmailMultiAlternatives(
+            subject=f'Greetings, {subscriber.username}, herea are the new articles from the last week.',
+            from_email='',
+            to=[subscriber.email]
+        )
+        msg.attach_alternative(html_content, 'text/html')
+        msg.send()
--- Newspaper/Newspaper/celery.py
+++ Newspaper/Newspaper/celery.py
@@ -1,9 +1,17 @@
 import os
 from celery import Celery
+from celery.schedules import crontab
  
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Newspaper.settings')
  
 app = Celery('Newspaper')
 app.config_from_object('django.conf:settings', namespace = 'CELERY')
+
+app.conf.beat_schedule = {
+    'weekly_mail': {
+        'task': 'NewsPortal.tasks.send_weekly_mail',
+        'schedule': crontab(hour=8, minute=0, day_of_week='monday'),
+    },
+}
 
 app.autodiscover_tasks()
